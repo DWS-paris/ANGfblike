85d886920ffa6672ac535d9fd1e66f0750f1edf7
DWS-paris <j.noyer@digitalworkshop.fr>
2017-02-22 15:31:47 +0100

    Premier commit : création du projet et ajout des routes

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -3,17 +3,29 @@ import { NgModule } from '@angular/core';
 import { FormsModule } from '@angular/forms';
 import { HttpModule } from '@angular/http';
 
+// Importer la constante des route
+import { Routing } from './routing';
+
 import { AppComponent } from './app.component';
+import { LoginComponent } from './components/login/login.component';
+import { ConnectedComponent } from './components/connected/connected.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    LoginComponent,
+    ConnectedComponent
   ],
+
   imports: [
     BrowserModule,
     FormsModule,
-    HttpModule
+    HttpModule,
+
+    // Ajouter la constante des routes
+    Routing
   ],
+
   providers: [],
   bootstrap: [AppComponent]
 })
